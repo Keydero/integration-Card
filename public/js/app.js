1c03a7a3f332ca31763da1a6c6f6fe0f04a86b93
Oussama Keddar <oussama.keddar@epitech.eu>
2018-04-01 18:41:37 +0200

    add animation to send/add button

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,8 @@ $(function() {
 	$('#switch').click(function() {
 		$(this).toggleClass('toggle');
 	});
-	
-	let progress = document.getElementById("progress").value = "0.55";
+
+	// let progress = document.getElementById("progress").value = "0.55";
 
 // $('#input').keydown(function () {
 //         $(this).toggleClass("is-filled", this.value != ' ');
